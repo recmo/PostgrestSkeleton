ac4b5b9e73bed289a5308d722bceb5817a504183
Remco Bloemen <remco-git@xn--2-umb.com>
2016-06-28 15:01:03 +0100

    Add logging

--- database/00_logging.sh
+++ database/00_logging.sh
@@ -1,4 +1,24 @@
+mkdir -p /var/log/postgresql
+chown postgres:postgres /var/log/postgresql
 echo "
+
+# LOG LOCATION
+
 log_destination = stderr
+logging_collector = true
+log_directory = '/var/log/postgresql'
+
+# ERROR REPORTING AND LOGGING
+
 log_statement = all
+log_min_duration_statement = 0
+log_line_prefix = '%t [%p]: [%l-1] user=%u,db=%d,app=%a,client=%h '
+log_checkpoints = on
+log_connections = on
+log_disconnections = on
+log_lock_waits = on
+log_temp_files = 0
+log_autovacuum_min_duration = 0
+log_duration = on
+
 " >> /var/lib/postgresql/data/postgresql.conf
