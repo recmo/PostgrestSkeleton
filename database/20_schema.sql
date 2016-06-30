f4b5a32cca3508f91747357857382c7f774ec174
Remco Bloemen <remco-git@xn--2-umb.com>
2016-06-30 20:44:09 +0100

    Fix Row level security policies

--- database/20_schema.sql
+++ database/20_schema.sql
@@ -26,9 +26,19 @@ GRANT USAGE, SELECT ON SEQUENCE posts_id_seq TO author;
 
 -- …but only of rows that he/she himself created.
 ALTER TABLE posts ENABLE ROW LEVEL SECURITY;
-CREATE POLICY author_eigenedit ON posts
-	USING (TRUE)
+
+CREATE POLICY select_unsecure ON posts FOR SELECT
+	USING (TRUE);
+
+CREATE POLICY author_eigencreate ON posts FOR INSERT
 	WITH CHECK (author = current_user_id());
+
+CREATE POLICY author_eigenupdate ON posts FOR UPDATE
+	USING (author = current_user_id())
+	WITH CHECK (author = current_user_id());
+
+CREATE POLICY author_eigendelete ON posts FOR DELETE
+	USING (author = current_user_id());
 
 --------------------------------------------------------------------------------
 
@@ -59,8 +69,18 @@ GRANT USAGE, SELECT ON SEQUENCE comments_id_seq TO author;
 
 -- …but only of comments that he/she himself created.
 ALTER TABLE comments ENABLE ROW LEVEL SECURITY;
-CREATE POLICY author_eigenedit ON comments
-	USING (TRUE)
+
+CREATE POLICY select_unsecure ON comments FOR SELECT
+	USING (TRUE);
+
+CREATE POLICY author_eigencreate ON comments FOR INSERT
 	WITH CHECK (author = current_user_id());
+
+CREATE POLICY author_eigenupdate ON comments FOR UPDATE
+	USING (author = current_user_id())
+	WITH CHECK (author = current_user_id());
+
+CREATE POLICY author_eigendelete ON comments FOR DELETE
+	USING (author = current_user_id());
 
 COMMIT;
