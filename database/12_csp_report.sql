66d89e8619f640e8db30170bb715670489430b7c
Remco Bloemen <remco-git@xn--2-umb.com>
2016-06-30 20:43:49 +0100

    Fix CSP reports

--- database/12_csp_report.sql
+++ database/12_csp_report.sql
@@ -1,18 +1,18 @@
 BEGIN;
 
 CREATE TABLE hidden.csp_reports (
-	id            text      primary key,
+	id            bigserial primary key,
 	created       timestamp not null
 	                        default now(),
 	report        jsonb     not null
 );
 
-CREATE FUNCTION csp_report(csp_report json) RETURNS void
+CREATE FUNCTION csp_report("csp-report" json) RETURNS void
 LANGUAGE SQL
 SECURITY DEFINER
 AS $$
 	INSERT INTO hidden.csp_reports (report)
-	VALUES (csp_report)
+	VALUES ("csp-report")
 $$;
 
 GRANT EXECUTE ON FUNCTION csp_report(json) TO anonymous;
